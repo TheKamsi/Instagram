a6aa951fe9aaa87e6f6c50b8d11ee3685ecf3b2a
Kamsiyochukwu Arinze <65101328+TheKamsi@users.noreply.github.com>
2022-03-26 00:33:06 -0500

    Instagram clone

---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by Kamsi Arinze on 3/22/22.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
